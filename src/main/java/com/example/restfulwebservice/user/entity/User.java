d54acc7748a241f0c35e5adbb08f9357ae2d6247
Hyoki Min <raniless@naver.com>
2022-10-03 22:36:48 +0900

    게시물 관리를 위한 Post Entity 추가와 초기 데이터 생성

--- src/main/java/com/example/restfulwebservice/user/entity/User.java
+++ src/main/java/com/example/restfulwebservice/user/entity/User.java
@@ -9,13 +9,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -26,6 +24,7 @@ import java.util.Date;
 @Entity
 @Table(name="users")
 public class User {
+
     @Id
     @GeneratedValue
     private Integer id;
@@ -43,4 +42,11 @@ public class User {
 //    @JsonIgnore
     @ApiModelProperty(notes = "사용자 주민번호를 입력해 주세요")
     private String ssn;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
+    public User(int id, String name, Date joinDate, String password, String ssn) {
+        this(id, name, joinDate, password, ssn, null);
+    }
 }
